0f6acac6d9403f082ce42f0ad4c8156cadc5f086
oldliu <57282807+jimmy90709@users.noreply.github.com>
2019-12-14 00:52:43 +0800

    作業4

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("血量")]
+    public float Hp = 100;
+    [Header("攻擊")]
+    public float Atk = 0;
+    [Header("殭屍")]
+    public Zomble Zb;
+    [Header("喇叭")]
+    public AudioSource Aud;
+    [Header("音效")]
+    public AudioClip SoundAtk;
+
+    public void A()  //攻擊
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Aud.PlayOneShot(SoundAtk, 1f);
+            Atk = Random.Range(0f, 40f);
+            if(Zb.Hp >= Atk)
+            {
+                Hurt();
+            }
+            else
+            {
+                Dead();
+            }
+        }
+    }
+     
+    public void Hurt()  //受傷
+    {
+
+        Zb.Hp = Zb.Hp - Atk;
+        print("<color=red>殭屍受到傷害:" + Atk + "</color>");
+        print("<color=red>殭屍剩下血量:" + Zb.Hp + "</color>");
+    }
+
+    public void Dead()  //死亡
+    {
+        Zb.Hp = Zb.Hp - Atk;
+        print("<color=red>殭屍受到傷害:" + Atk.ToString("F2") + "</color>");
+        print("<color=red>殭屍剩下血量:" + Zb.Hp.ToString("F2") + "</color>");
+        print("<color=red>殭屍死亡" + "</color>");
+    }
+    private void FixedUpdate()
+    {
+        A();
+    }
+
+}
---
+++ UnityProject/Assets/Zomble.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;//UnityEngine.Random
+
+public class Zomble : MonoBehaviour
+{
+    [Header("血量")]
+    public float Hp = 50;
+    [Header("攻擊")]
+    public float Atk = 0;
+    [Header("玩家")]
+    public Player Player;
+    [Header("喇叭")]
+    public AudioSource Aud;
+    [Header("音效")]
+    public AudioClip SoundAtk;
+
+    private void A()  //攻擊
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Aud.PlayOneShot(SoundAtk, 1f);
+            Atk = UnityEngine.Random.Range(0f, 40f);
+            if (Player.Hp >= Atk)
+            {
+                Hurt();
+            }
+            else
+            {
+                Dead();
+            }
+        }
+    }
+
+    
+    public void Hurt()  //受傷
+    {
+        Player.Hp = Player.Hp - Atk;
+        print("<color=blue>玩家受到傷害:" + Atk+"</color>");
+        //print("<color=#FF0000FF>"+"玩家受到傷害:" + Atk + "</color>");
+         
+        print("<color=blue>玩家剩下血量:" + Player.Hp + "</color>");
+    }   
+      public void Dead()  //死亡
+    {/*
+        <color =#123456FF></color>
+         
+         RGBA = FF FF FF FF
+         R = red 
+         G = green
+         B = blue 
+         A = alpha
+         */
+
+
+
+
+        Player.Hp = Player.Hp - Atk;
+        print("<color=blue>玩家受到傷害:" + Atk + "</color>");
+        print("<color=blue>玩家剩下血量:" + Player.Hp + "</color>");
+        print("<color=blue>玩家死亡" + "</color>");
+    }
+
+
+    private void FixedUpdate()
+    {
+        A();
+    }
+}
